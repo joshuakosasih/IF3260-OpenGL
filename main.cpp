187079ae41ac66395ec0553706e5b96787a84ed6
SimpliCty <nikolas.wangsaputra@gmail.com>
2017-04-11 23:57:53 +0700

    coloring

--- main.cpp
+++ main.cpp
@@ -10,13 +10,17 @@
 
 #include <stdio.h>
 #include <cmath>
+#include <iostream>
 
 float counter = 0.0;
+float red[3] = {1.0, 0.0, 0.0};
+float white[3] = {1.0, 1.0, 1.0};
+float black[3] = {0.0, 0.0, 0.0};
 
 void circle(float x, float y, float r, int anglestart, int anglestop) {
     for (int i = anglestart;i < anglestop ;i++) {
         float degInRad = i*3.14159/180;
-        glColor3f(1, 1, 1);
+        //glColor3f(1, 1, 1);
         glVertex2f(cos(degInRad)*r + x, sin(degInRad)*r + y);
     }
 }
@@ -24,92 +28,152 @@ void circle(float x, float y, float r, int anglestart, int anglestop) {
 void InverseCircle(float x, float y, float r, int anglestart, int anglestop) {
     for (int i = anglestart-1;i >= anglestop ;i--) {
         float degInRad = i*3.14159/180;
-        glColor3f(1, 1, 1);
+        //glColor3f(1, 1, 1);
         glVertex2f(cos(degInRad)*r + x, sin(degInRad)*r + y);
+    }
+}
+
+void floodfill(float pointx,float pointy,float f[3],float o[3])
+{
+    float intensity,pixels[3];
+    glReadPixels(pointx,pointy,1.0,1.0,GL_RGB,GL_FLOAT,pixels);
+    if(pixels[0]==o[0] && (pixels[1])==o[1] && (pixels[2])==o[2])
+    {
+        std::cout << "AA" << std::endl;
+        glFlush();
+        glBegin(GL_POINTS);
+        glColor3fv(f);
+        glVertex2i(pointx,pointy);
+        glEnd();
+        glFlush();
+        floodfill(pointx+1,pointy,f,o);
+        floodfill(pointx,pointy+1,f,o);
+        floodfill(pointx-1,pointy,f,o);
+        floodfill(pointx,pointy-1,f,o);
     }
 }
 
 void drawDragonTail() {
     glLineWidth(2.5);
-    glBegin(GL_LINES);
-        glColor3f(1.0, 1.0, 1.0);
-        glVertex2f(-0.18, -0.22);
+    glBegin(GL_POLYGON);
+        glColor3fv(red);
+        glVertex2f(-0.18, -0.02);
         glVertex2f(0, -0.7);
+        glVertex2f(0.05, -0.02);
     glEnd();
-    glBegin(GL_LINE_STRIP);
-        circle(-0.18, -0.7, 0.18, 90, 360);
+
+    glBegin(GL_POLYGON);
+        glColor3fv(red);
+        circle(-0.14, -0.68, 0.25, 180, 360);
     glEnd();
-    glBegin(GL_LINE_STRIP);
-        circle(-0.14, -0.7, 0.25, 180, 360);
+    glBegin(GL_POLYGON);
+        glColor3fv(red);
+        circle(-0.19, -0.69, 0.2, 90, 180);
     glEnd();
-    glBegin(GL_LINE_STRIP);
-        circle(-0.19, -0.71, 0.2, 90, 180);
+    glBegin(GL_POLYGON);
+        glColor3fv(black);
+        circle(-0.18, -0.68, 0.18, 90, 360);
     glEnd();
-    glBegin(GL_LINES);
+    //glBegin(GL_LINE_STRIP);
+        //circle(-0.14, -0.7, 0.25, 180, 360);
+    //glEnd();
+    //glBegin(GL_LINE_STRIP);
+        //circle(-0.19, -0.71, 0.2, 90, 180);
+    //glEnd();
+    glBegin(GL_POLYGON);
+        glColor3fv(red);
+        glVertex2f(0, -0.7);
         glVertex2f(0.11, -0.7);
-        glVertex2f(0.05, -0.22);
+        //floodfill(0,-0.23,red,red);
+        glVertex2f(0.05, -0.02);
     glEnd();
 }
 
 void drawDragonHead() {
     glLineWidth(2.5);
-    glBegin(GL_LINES);
-        glColor3f(1.0, 1.0, 1.0);
+    glColor3fv(red);
+    //glBegin(GL_LINE_STRIP);
+    glBegin(GL_POLYGON);
+        //glColor3f(1.0f, 0.0f, 0.0f);
         glVertex2f(0.2, 0.5);
         glVertex2f(0.15, 0.47);
 
-        glVertex2f(0.15, 0.47);
+        //glVertex2f(0.15, 0.47);
         glVertex2f(0.05, 0.55);
 
-        glVertex2f(0.05, 0.55);
+        //glVertex2f(0.05, 0.55);
         glVertex2f(0.05, 0.57);
 
         glVertex2f(0.2, 0.5);
         glVertex2f(0.1, 0.7);
 
-        glVertex2f(0.1, 0.7);
+        //glVertex2f(0.1, 0.7);
         glVertex2f(0.05, 0.7);
 
         //Right Ear
-        glVertex2f(0.05, 0.7);
+        //glVertex2f(0.05, 0.7);
         glVertex2f(0.1, 0.75);
 
-        glVertex2f(0.1, 0.75);
+        //glVertex2f(0.1, 0.75);
         glVertex2f(0.07, 0.9);
 
-        glVertex2f(0.07, 0.9);
+        //glVertex2f(0.07, 0.9);
         glVertex2f(0.08, 0.77);
 
-        glVertex2f(0.08, 0.77);
+        //glVertex2f(0.08, 0.77);
         glVertex2f(0.04, 0.7);
 
-        glVertex2f(0.04, 0.7);
+        //glVertex2f(0.04, 0.7);
         glVertex2f(0.01, 0.7);
 
         //Left Ear
-        glVertex2f(0.01, 0.7);
+        //glVertex2f(0.01, 0.7);
         glVertex2f(0.04, 0.75);
 
-        glVertex2f(0.04, 0.75);
+        //glVertex2f(0.04, 0.75);
         glVertex2f(0.02, 0.9);
 
-        glVertex2f(0.02, 0.9);
+        //glVertex2f(0.02, 0.9);
         glVertex2f(0.018, 0.75);
 
-        glVertex2f(0.018, 0.75);
+        //glVertex2f(0.018, 0.75);
         glVertex2f(0, 0.7);
     glEnd();
 
-    //Outer neck
-    glBegin(GL_LINE_STRIP);
-    circle(0.05, 0.455, 0.25, 90, 200);
+    glBegin(GL_POLYGON);
+        glColor3fv(red);
+        glVertex2f(0.2, 0.5);
+        glVertex2f(0.05, 0.7);
+        glVertex2f(-0.1, 0.6);
     glEnd();
 
-    glBegin(GL_LINE_STRIP);
-    circle(0.02, 0.47, 0.1, 70, 250);
+    //Outer neck
+    glBegin(GL_POLYGON);
+    glColor3fv(red);
+    circle(0.05, 0.455, 0.25, 75, 230);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3fv(black);
+    circle(0.02, 0.45, 0.1, 70, 250);
     glEnd();
 }
+void drawWingTry(){
+    glBegin(GL_POLYGON);
+        glColor3fv(red);
+        circle(0, 0, 1, -45, 225);
+    glEnd();
+    glBegin(GL_POLYGON);
+        glColor3fv(black);
+        circle(0, 0.9, 0.55, 30, 355);
+    glEnd();
+    glBegin(GL_POLYGON);
+        glColor3fv(black);
+        circle(0, -0.75, 0.7, -45, 225);
+    glEnd();
 
+
+}
 void drawRightWing() {
     /*glBegin(GL_LINE_STRIP);
     circle(-0.18,0.65,0.28, 140, 270);
@@ -118,7 +182,7 @@ void drawRightWing() {
     glBegin(GL_LINE_STRIP);
     circle(-0.39,0.055,0.78, 90, 180);
     glEnd();*/
-
+    glColor3fv(red);
     glBegin(GL_LINE_STRIP);
     circle(-0.06,0.65,0.28, 140, 245);
     glEnd();
@@ -132,7 +196,7 @@ void drawRightWing() {
     glEnd();
 
     glBegin(GL_LINES);
-        glColor3f(1.0, 1.0, 1.0);
+        glColor3fv(red);
         glVertex2f(-0.872, 0.12);
         glVertex2f(-0.78, -0.2);
     glEnd();
@@ -142,7 +206,7 @@ void drawRightWing() {
     glEnd();
 
     glBegin(GL_LINES);
-        glColor3f(1.0, 1.0, 1.0);
+        glColor3fv(red);
         glVertex2f(-0.60, -0.135);
         glVertex2f(-0.38, -0.4);
     glEnd();
@@ -165,8 +229,9 @@ void drawRightWing() {
 }
 
 void drawLeftWing() {
+    glColor3fv(red);
     glBegin(GL_LINE_STRIP);
-    circle(0.0,0.65,0.28, -90, 42);
+    circle(-0.0,0.65,0.28, -90, 42);
     glEnd();
 
     glBegin(GL_LINE_STRIP);
@@ -178,7 +243,7 @@ void drawLeftWing() {
     glEnd();
 
     glBegin(GL_LINES);
-        glColor3f(1.0, 1.0, 1.0);
+        glColor3fv(red);
         glVertex2f(0.812, 0.12);
         glVertex2f(0.72, -0.2);
     glEnd();
@@ -188,7 +253,7 @@ void drawLeftWing() {
     glEnd();
 
     glBegin(GL_LINES);
-        glColor3f(1.0, 1.0, 1.0);
+        glColor3fv(red);
         glVertex2f(0.54, -0.135);
         glVertex2f(0.32, -0.4);
     glEnd();
@@ -212,10 +277,9 @@ void drawLeftWing() {
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
+    drawWingTry();
     drawDragonHead();
     drawDragonTail();
-    drawRightWing();
-    drawLeftWing();
     glutSwapBuffers();
 }
 
@@ -236,7 +300,6 @@ int main(int argc, char **argv) {
     glutInitWindowSize(900, 600);
     glutInitWindowPosition(200, 50);
     glutCreateWindow("Second OpenGL App");
-
     initOpenGL();
 
     glutDisplayFunc(display);
