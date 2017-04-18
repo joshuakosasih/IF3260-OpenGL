dd7bf24a3261810156360efec2425affa36157e8
Joshua A Kosasih <joshua.kosasih@gmail.com>
2017-04-18 10:45:55 +0700

    separate CALLBACK codes

--- main.cpp
+++ main.cpp
@@ -17,16 +17,15 @@
 #include <sstream>
 #include <iomanip>
 #include <cmath>
+#include "callback.h"
 
 using std::stringstream;
 using std::cout;
-using std::cerr;
-using std::endl;
 using std::ends;
 
 float counter = 0.0;
 float red = 1, green = 1, blue = 1;
-
+/*
 // CALLBACK functions for GLU_TESS ////////////////////////////////////////////
 // NOTE: must be declared with CALLBACK directive
 void CALLBACK tessBeginCB(GLenum which);
@@ -36,10 +35,9 @@ void CALLBACK tessVertexCB(const GLvoid *data);
 void CALLBACK tessVertexCB2(const GLvoid *data);
 void CALLBACK tessCombineCB(const GLdouble newVertex[3], const GLdouble *neighborVertex[4],
                             const GLfloat neighborWeight[4], GLdouble **outData);
-
+*/
 GLdouble quads[700][6];
-GLdouble vertices[64][6];
-int vertexIndex = 0;
+
 
 void circle(float x, float y, float r, int anglestart, int anglestop) {
     for (int i = anglestart;i < anglestop ;i++) {
@@ -361,73 +359,4 @@ int main(int argc, char **argv) {
     glutMainLoop();
 
     return 0;
-}
-
-void CALLBACK tessBeginCB(GLenum which)
-{
-    glBegin(which);
-}
-void CALLBACK tessEndCB()
-{
-    glEnd();
-}
-void CALLBACK tessVertexCB(const GLvoid *data)
-{
-    // cast back to double type
-    const GLdouble *ptr = (const GLdouble*)data;
-
-    glVertex3dv(ptr);
-}
-void CALLBACK tessVertexCB2(const GLvoid *data)
-{
-    // cast back to double type
-    const GLdouble *ptr = (const GLdouble*)data;
-
-    glColor3dv(ptr+3);
-    glVertex3dv(ptr);
-
-    // DEBUG //
-    //cout << "  glColor3d(" << *(ptr+3) << ", " << *(ptr+4) << ", " << *(ptr+5) << ");\n";
-    //cout << "  glVertex3d(" << *ptr << ", " << *(ptr+1) << ", " << *(ptr+2) << ");\n";
-}
-void CALLBACK tessCombineCB(const GLdouble newVertex[3], const GLdouble *neighborVertex[4],
-                            const GLfloat neighborWeight[4], GLdouble **outData)
-{
-    // copy new intersect vertex to local array
-    // Because newVertex is temporal and cannot be hold by tessellator until next
-    // vertex callback called, it must be copied to the safe place in the app.
-    // Once gluTessEndPolygon() called, then you can safly deallocate the array.
-    vertices[vertexIndex][0] = newVertex[0];
-    vertices[vertexIndex][1] = newVertex[1];
-    vertices[vertexIndex][2] = newVertex[2];
-
-    // compute vertex color with given weights and colors of 4 neighbors
-    // the neighborVertex[4] must hold required info, in this case, color.
-    // neighborVertex was actually the third param of gluTessVertex() and is
-    // passed into here to compute the color of the intersect vertex.
-    vertices[vertexIndex][3] = neighborWeight[0] * neighborVertex[0][3] +   // red
-                               neighborWeight[1] * neighborVertex[1][3] +
-                               neighborWeight[2] * neighborVertex[2][3] +
-                               neighborWeight[3] * neighborVertex[3][3];
-    vertices[vertexIndex][4] = neighborWeight[0] * neighborVertex[0][4] +   // green
-                               neighborWeight[1] * neighborVertex[1][4] +
-                               neighborWeight[2] * neighborVertex[2][4] +
-                               neighborWeight[3] * neighborVertex[3][4];
-    vertices[vertexIndex][5] = neighborWeight[0] * neighborVertex[0][5] +   // blue
-                               neighborWeight[1] * neighborVertex[1][5] +
-                               neighborWeight[2] * neighborVertex[2][5] +
-                               neighborWeight[3] * neighborVertex[3][5];
-
-
-    // return output data (vertex coords and others)
-    *outData = vertices[vertexIndex];   // assign the address of new intersect vertex
-
-    ++vertexIndex;  // increase index for next vertex
-}
-void CALLBACK tessErrorCB(GLenum errorCode)
-{
-    const GLubyte *errorStr;
-
-    errorStr = gluErrorString(errorCode);
-    cerr << "[ERROR]: " << errorStr << endl;
 }
